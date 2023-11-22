86378243bf9979d207443a416a08064537c8969c
tmccormack165 <60988784+tmccormack165@users.noreply.github.com>
2023-11-22 11:05:47 -0500

    Add files via upload

---
+++ index.php
@@ -0,0 +1,20 @@
+<?php
+
+// Define an associative array with information about Riga
+$rigaInfo = [
+    'name' => 'Riga',
+    'country' => 'Latvia',
+    'population' => 632,614,
+    'area' => 307.17,
+    'language' => 'Latvian',
+    'currency' => 'Euro',
+];
+
+// Output the information about Riga
+echo "Name: " . $rigaInfo['name'] . "\n";
+echo "Country: " . $rigaInfo['country'] . "\n";
+echo "Population: " . $rigaInfo['population'] . "\n";
+echo "Area: " . $rigaInfo['area'] . " square kilometers\n";
+echo "Language: " . $rigaInfo['language'] . "\n";
+echo "Currency: " . $rigaInfo['currency'] . "\n";
+?>
